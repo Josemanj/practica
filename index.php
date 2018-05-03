d2bb614213c06a7edd51d7c5950f3f447a02f804
Jose J <jmany@hotmail.com>
2018-05-02 21:37:11 -0500

    boton

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
+$nombre=$_POST["nombre"];
   echo "aprendiendo";
-  ?>
-  <center>
-   <font color="blue" size="6">Aprendiendo GitHub</font>
-  </center>
-  <?php
+  echo "<br> Nombre: ".$nombre;
 ?>
